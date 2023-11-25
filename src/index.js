ebc10d97614659b56f55a1586b58bb286d0ba921
ashish <ashish01@nagarro.com>
2023-11-25 16:05:58 +0530

    try

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from "./App";
+import {AbortController} from "node-abort-controller";
+
+global.AbortController = AbortController;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,9 @@
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
 import '@/styles/globals.css'
-import { Montserrat } from 'next/font/google';
 import Head from 'next/head'
 
 
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: "--font mont"
-})
 
 export default function App({ Component, pageProps }) {
   return (
@@ -17,7 +12,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <main className={`font-mont bg-light w-full min-h-screen`}>
         <NavBar />
         <Component {...pageProps} />
       </main>
